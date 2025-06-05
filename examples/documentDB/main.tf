9c2fda8baf9534bef3a94dd795862189fa35997d
Steven Stone <steven.stone@clickhouse.com>
2025-06-05 11:01:50 -0400

    Update documentation

--- examples/documentDB/main.tf
+++ examples/documentDB/main.tf
@@ -7,7 +7,6 @@ terraform {
     }
   }
 }
-
 provider "mongodb" {
   host = "documentdb-test-terraform.cluster-ro-ctclcdufsrkx.eu-west-3.docdb.amazonaws.com"
   port = "27017"
@@ -17,7 +16,6 @@ provider "mongodb" {
   direct = true
   certificate = file(pathexpand("rds-combined-ca-bundle.pem"))
 }
-
 resource "mongodb_db_user" "user" {
   auth_database = "admin"
   name = "monta"
@@ -35,11 +33,10 @@ resource "mongodb_db_user" "user" {
     db =   "monta"
   }
 }
-
 # https://docs.aws.amazon.com/documentdb/latest/developerguide/iam-identity-auth.html#iam-identity-auth-get-started
 resource "mongodb_db_user" "passwordless_user" {
-  auth_database = "$external"
-  auth_mechanisms = ["MONGODB-AWS"]
+  auth_database = var.auth_database
+  auth_mechanisms = var.auth_mechanisms
   name = "arn:aws:iam::123456789123:user/iamuser"
   role {
     role = "readAnyDatabase"
